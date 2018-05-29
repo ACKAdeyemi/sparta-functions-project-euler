10b95ac68357e1b08871e84080307f6d0e1bc6f1
Tech-A43 <tech-a43@tech-a43s-air.lan>
2018-05-30 00:45:49 +0100

    Attempt to resolve conflicts

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 console.log('Working');
+// 1. Write a function called 'multiply' that multiplies two numbers and returns the result
+function multiply () {
+
+}
+
+// 2. Write a function called 'addThree' that adds three numbers together and returns the result
+
+// 3. Write a function called 'smallestNumber' that returns the smaller of 2 numbers
+
+// 4. Write a function called 'maxOfThree' that returns the largest of 3 numbers
+
+// 5. Write a function called 'reverseString' that returns the reverse a string
+
+// 6. Write a function called 'disemvowel' that returns the vowels from the string parameter
+
+// 7. Write a function called 'removeOdd' that removes all ODD number from an array
+
+// 8. Write a function called 'removeEven' that removes all EVEN number from an array
+
+// 9. Write a function called 'longestString' that takes an array of strings and returns the string with the longest character length
+
+// 10. Write a function called 'allElementsExceptFirstThree' that discards the first 3 elements of an array,
+// e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
+
 //#### BONUS ####
 
 // 11. Write a function called 'convertArrayToAnObject' that turns an array (with an even number of elements) into a hash, by
